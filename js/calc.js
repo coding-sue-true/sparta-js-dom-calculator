a6edf638b0aaa9f0f807b034f260e927055169ba
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-11-01 20:58:23 +0000

    calc.js - variables set for .button and .operator // calc.html - buttons replaced for input with values and types

---
+++ js/calc.js
@@ -0,0 +1,24 @@
+//
+var buttonNum = document.querySelectorAll('.button');
+var operation = document.querySelectorAll('.operator');
+
+
+
+
+
+// adding the event listener
+// buttonNum.addEventListener('click', numberClicked);
+//
+// function numberClicked() {
+//   console.log('button was clicked via function');
+//
+//
+//   debugger
+//   return buttonNum;
+// }
+
+// function to store the value of the clicked button
+
+
+// Display current number
+// screen.innerHTML = theNum;
