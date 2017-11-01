db335f2a1cf4b765d87d63f2ac1bd447b3af5f88
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-11-01 22:16:31 +0000

    calc.html&cal.css  is now correct, calculator on the browser is ok // calc.js - global var declared, operator and number listeners into functions

--- js/calc.js
+++ js/calc.js
@@ -1,37 +1,45 @@
+var firstNumber = true;
+var secondNumber = true;
+var operator;
+
+//number clicked in the calculator
+function setNumberListeners() {
+  //get the input from the user
+  var btn = document.querySelectorAll('.buttonNum');
+
+  for (var i = 0; i < btn.length; i++) {
+    var numberClicked = btn[i];
+
+    numberClicked.addEventListener('click', function(event) {
+      console.log(this.value + ' was clicked');
+    })
+  }
+};
+
+//operator clicked in the calculator
+function setOperatorListeners(){
+  //get the input from the user
+  var operation = document.querySelectorAll('.operator');
+
+  for (var i = 0; i < operation.length; i++) {
+    var operator = operation[i];
+
+    operator.addEventListener('click', function(event) {
+      console.log(this.value + ' was clicked');
+    })
+  }
+
+//   var clear = document.querySelectorAll('.buttonClear');
 //
-var buttonNum = document.querySelectorAll('.button');
-var operation = document.querySelectorAll('.operator');
+// };
 
-for (var i = 0; i < buttonNum.length; i++) {
-  var numberClicked = buttonNum[i];
-
-  numberClicked.addEventListener('click', function(event) {
-    console.log(this.value + ' was clicked');
-  })
-}
-
-for (var i = 0; i < operation.length; i++) {
-  var calc = operation[i];
-
-  calc.addEventListener('click', function(event) {
-    console.log(this.value + ' was clicked');
-  })
-}
-
-
-// adding the event listener
-// buttonNum.addEventListener('click', numberClicked);
+setNumberListeners();
+setOperatorListeners();
+// function calculation(){
 //
-// function numberClicked() {
-//   console.log('button was clicked via function');
-//
-//
-//   debugger
-//   return buttonNum;
 // }
 
-// function to store the value of the clicked button
 
 
 // Display current number
-// screen.innerHTML = theNum;
+// screen.innerHTML = calc;
